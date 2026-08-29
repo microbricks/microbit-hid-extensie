f8b0ff35d201375499d3076aa5691fe2bcbcb3b1
microbricks <nickvanzweden@gmail.com>
2026-08-29 10:37:05 +0200

    Create shims.d.ts

---
+++ shims.d.ts
@@ -0,0 +1,8 @@
+//% shim=hid
+namespace hid {
+    //% shim=hid.sendConsumer
+    function sendConsumer(code: number): void;
+
+    //% shim=hid.sendKeyboard
+    function sendKeyboard(text: string): void;
+}
